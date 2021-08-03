a5677d37134afe2d464afb9b69a72b6a80991040
Aaqib21 <shaikhaaqib12@yahoo.com>
2021-08-03 21:06:52 +0100

    First Commit

---
+++ src/test/java/com/AkberShaikh/Website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AkberShaikh.Website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
